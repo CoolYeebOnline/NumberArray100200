eceb4645a742f4f03197389549e0a73fad4e05dc
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-10-22 18:12:04 +0100

    Number Array 0-100 and 100-200

--- NumberArrays/NumberArrays/Source.cpp
+++ NumberArrays/NumberArrays/Source.cpp
@@ -6,19 +6,31 @@ int main() {
 
 	const int arraySize = 100;
 	const int arraySize2 = 100;
-	const int arraySize3 = 100;
 	int array1[arraySize];
 	int array2[arraySize2];
-	int array3[arraySize3];
+	int array3[arraySize*2];
 
 	for (int i = 0; i < 100; i++) {
 
 		array1[i] = i + 1;
+		array2[i] = i + 100;
+	
 
-		std::cout << array1[i] << std::endl;
+		std::cout << array1[i] << '\n' << array2[i] << std::endl;
 	}
 
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0, j = 99, k = 0; i < arraySize; i++, j--) {
+		array3[k] = array1[i];
+		k++;
+
+		array3[k] = array2[j];
+		k++;
+
+	}
+
+	int i = 0;
+
+	/*for (int i = 0; i < 100; i++) {
 
 		array2[i] = i + 100;
 
@@ -27,9 +39,9 @@ int main() {
 
 	for (int i = 0; i < 100; i++) {
 
-		std::cout << array2[i] << '\n' << array1[i]  << std::endl;
+		std::cout <<	array2[i] << '\n' << array1[i]  << std::endl;
 		
-	}
+	//*/
 
 	//if (i = 100) {
 	//	for (int i = 100; i < 200; i++) {
@@ -38,7 +50,7 @@ int main() {
 	//	}
 	//}
 
-	return 0;
+	/*return 0;*/
 
 	
 	
